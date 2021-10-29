1a5e3daeae20ca7335f361c9976040e7585049a6
Shivammishra789 <shivammishra7789@gmail.com>
2021-10-29 23:30:54 +0530

    inserting node between two nodes

--- com/linkelist/creation/LinkedList.java
+++ com/linkelist/creation/LinkedList.java
@@ -22,19 +22,43 @@ public class LinkedList {
 	public void appendNode(int data) {
 		Node tail;
 		Node myNode = new Node();
-		myNode.setData(data); 
-		if (head == null) {
-			head = myNode; // assigning value of first node to head
-			tail = myNode;
+		myNode.setData(data);
+		if(head == null) {
+			head = myNode;
 		}
 		else {
 			tail = head;
 			while(tail.getNext() != null) {
 				tail = tail.getNext();
 			}
-			tail.setNext(myNode);	
+			tail.setNext(myNode);
 		}
 		size++;
+	}
+	
+	// method to add node at given position 
+	public void insertAtPos(int pos, int data) {
+		if(pos == 1) {
+			addNode(data);
+		}
+		else if(pos == size+1) {
+			appendNode(data);
+		}
+		else if(pos > 1 && pos <= size) {
+			Node temp;
+			temp = head;
+			Node myNode = new Node();
+			myNode.setData(data);
+			for(int i = 1; i < pos-1 ; i++) {
+				temp = temp.getNext();
+			}
+			myNode.setNext(temp.getNext());
+			temp.setNext(myNode);	
+		}
+		else {
+			System.out.println("entered position out of range");
+			}
+		size++;	
 	}
 
 	// method to display nodes
@@ -54,9 +78,9 @@ public class LinkedList {
 	
 	public static void main(String[] args) {
 		LinkedList numList = new LinkedList();
-		numList.appendNode(70);
-		numList.appendNode(30);
 		numList.appendNode(56);
+		numList.appendNode(70);
+		numList.insertAtPos(2,30);
 		numList.display();
 	}
 }
